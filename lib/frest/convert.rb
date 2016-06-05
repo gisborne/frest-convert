0fd5a1fcab237c654001df72aea442d4bf12498e
Guyren Howe <guyren@relevantlogic.com>
2016-06-04 23:29:11 -0700

    Initial checkin

---
+++ lib/frest/convert.rb
@@ -0,0 +1,24 @@
+require "frest/convert/version"
+
+module Frest
+  module Convert
+    @converters = {}
+
+    def register_converter converter:, from: [], to:
+        [*from].product([*to]).each do |f, t|
+          @converters[f]    ||= {}
+          @converters[f][t] ||= []
+          @converters[f][t] << converter
+        end
+    end
+
+    def convert from:, to:
+      p "from #{from.class}, to: #{to.inspect} converters: " + @converters.inspect
+      converter = @converters[from.class][to]
+      converter.last.call source: from, to: to
+    end
+
+    module_function :register_converter
+    module_function :convert
+  end
+end
---
+++ lib/frest/convert/version.rb
@@ -0,0 +1,5 @@
+module Frest
+  module Convert
+    VERSION = "0.0.1"
+  end
+end
